c07de90ed1198d5d3cc6dfb9386492d7f08165c8
Sumedh Pendurkar <sumedh.pendurkar@gmail.com>
2017-06-20 22:43:05 +0530

    added deconv layers;after day1 testing; to still process the data]

--- model.py
+++ model.py
@@ -7,14 +7,17 @@ from keras.optimizers import SGD
 
 import cv2
 import os
-from keras.layers import Conv2D, Dense
+from keras.layers import Conv2D, Dense, Deconv2D
+import sys
 
 def get_model_convolutional():
     model = keras.models.Sequential()
-    model.add(Conv2D(128, (3, 3), activation='relu', strides = (1,1), input_shape=(1028, 1028, 3)))
-    model.add(Conv2D(3, (3, 3), strides = (1,1), activation=None)) 
+    model.add(Conv2D(128, (3, 3), activation='relu', strides = (1,1), input_shape=(1024, 1024, 3)))
+    model.add(Conv2D(64, (3, 3), strides = (1,1), activation='relu'))
+    model.add(Deconv2D(64, (3, 3), strides = (1,1), activation = 'relu'))
+    model.add(Deconv2D(3, (3,3), strides = (1,1), activation = None))
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(loss='categorical_crossentropy', optimizer=sgd)
+    model.compile(loss='mse', optimizer=sgd)
     return model
 
 def get_model_ann():
@@ -24,12 +27,11 @@ def get_model_ann():
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(loss='categorical_crossentropy', optimizer=sgd)
     return model
-
-def training_x():
+def training_set():
     path_x = './dataset/x/'
     path_y = './dataset/y/'
     files = os.listdir(path_x)
-    max_img_size_x = 1028
+    max_img_size_x = 1024
     max_img_size_y = 1024
     train_x = np.zeros((108,max_img_size_x, max_img_size_x, 3), dtype = np.uint8)
     train_y = np.zeros((108,max_img_size_y, max_img_size_y, 3), dtype = np.uint8)
@@ -46,6 +48,7 @@ def training_x():
         for j in range(len(img)):
             for k in range(len(img[j])):
                 train_y[i][j][k][:] = img[j][k]
+        print(train_x[i].shape, train_y[i].shape)
     print(train_x.shape)
     print(train_y.shape)
     return train_x, train_y
@@ -54,10 +57,13 @@ if __name__ == '__main__':
     model1 = get_model_convolutional()
     #model2 = get_model_ann()
     model1.summary()
-    x_train, y_train = training_x()
+    x_train, y_train = training_set()
     print("x_train.size = ", x_train.size)
     print("y_train.size = ", y_train.size)
     print("x_train.shape = ", x_train.shape)
     print("y_train.shape = ", y_train.shape)
-    model1.fit(x_train, y_train, verbose = 2,epochs=20, batch_size=4)
-
+    model1.fit(x_train, y_train, verbose = 2,epochs=10, batch_size=4)
+    model1.save('sr_deconv_net.h5')
+    y_predicted = model1.predict(x_train[0:5], batch_size = 5)
+    for i in range(5):
+        cv2.imwrite('predicted_' + str(i), np.uint8(y_predicted[0]))
